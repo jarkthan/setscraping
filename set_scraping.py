ed5993a465b726fbd48571d8bf9a2f17584bdecd
Jarkthan <thanakorn.jark@gmail.com>
2023-02-19 18:42:25 +0700

    d

---
+++ set_scraping.py
@@ -0,0 +1,6 @@
+#import the necessary libraries
+import numpy as np
+import pandas as pd
+from selenium import webdriver
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
